29d6d5f3899eb500f441399a924448e85e988cf2
Hazrat Ali <iloveyii@yahoo.com>
2021-11-15 20:39:38 +0100

    Added readme.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# Nightwatch
+
+- Is a test runner and supports multi environments and browsers
+
+# Install
+
+- Install node packages `npm i`
+- Install npx globally `npm i -g npx`
+- Run all tests `npx nightwatch`
+
+# Tips
+
+- Error on Ubuntu 20.04 while using older chrome v 93.x: An error occurred while retrieving a new session: "session not created: This version of ChromeDriver only supports Chrome version 95"
+  - Solution : Update chrome to latest version
--- tests/ecosia.js
+++ tests/ecosia.js
@@ -1,14 +1,10 @@
-module.exports = {
-  "Demo test ecosia.org": function (browser) {
+describe("Ecosia", function () {
+  test("demo test", function (browser) {
     browser
       .url("https://www.ecosia.org/")
-      .waitForElementVisible("body")
-      .assert.titleContains("Ecosia")
-      .assert.visible("input[type=search]")
       .setValue("input[type=search]", "nightwatch")
-      .assert.visible("button[type=submit]")
       .click("button[type=submit]")
       .assert.containsText(".mainline-results", "Nightwatch.js")
       .end();
-  },
-};
+  });
+});
